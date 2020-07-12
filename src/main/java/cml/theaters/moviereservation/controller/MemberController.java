c8e80d14d819a37784960807a4ccc54f8f3261dd
sklim0932 <sklim0932@gmail.com>
2020-07-12 17:05:02 +0900

    UPDATE 멤버 전체 목록 반환 컨트롤러 변경

--- src/main/java/cml/theaters/moviereservation/controller/MemberController.java
+++ src/main/java/cml/theaters/moviereservation/controller/MemberController.java
@@ -3,8 +3,6 @@ package cml.theaters.moviereservation.controller;
 import cml.theaters.moviereservation.dto.MemberListResponseDto;
 import cml.theaters.moviereservation.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +15,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/member")
-    public ResponseEntity<MemberListResponseDto> memberList() {
-        List<MemberListResponseDto> memberListResponseDtoList = memberService.findAll();
-
-        return new ResponseEntity<>(memberListResponseDtoList.get(0), HttpStatus.OK);
+    public List<MemberListResponseDto> memberList() {
+        return memberService.findAll();
     }
 }
