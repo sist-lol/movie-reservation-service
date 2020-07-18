6474305be0b494ad8aad6a8ccdf4e3d2a63a02bc
sklim0932 <sklim0932@gmail.com>
2020-07-18 18:50:34 +0900

    ADD save() method to MemberController and MemberService

--- src/main/java/cml/theaters/moviereservation/controller/MemberController.java
+++ src/main/java/cml/theaters/moviereservation/controller/MemberController.java
@@ -1,9 +1,12 @@
 package cml.theaters.moviereservation.controller;
 
 import cml.theaters.moviereservation.dto.MemberListResponseDto;
+import cml.theaters.moviereservation.dto.MemberSignUpRequestDto;
 import cml.theaters.moviereservation.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +20,10 @@ public class MemberController {
     @GetMapping("/member")
     public List<MemberListResponseDto> memberList() {
         return memberService.findAll();
+    }
+
+    @PostMapping("/member")
+    public Long save(@RequestBody MemberSignUpRequestDto requestDto) {
+        return memberService.save(requestDto);
     }
 }
--- src/main/java/cml/theaters/moviereservation/service/member/MemberService.java
+++ src/main/java/cml/theaters/moviereservation/service/member/MemberService.java
@@ -1,7 +1,9 @@
 package cml.theaters.moviereservation.service.member;
 
+import cml.theaters.moviereservation.domain.member.Member;
 import cml.theaters.moviereservation.domain.member.MemberRepository;
 import cml.theaters.moviereservation.dto.MemberListResponseDto;
+import cml.theaters.moviereservation.dto.MemberSignUpRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,5 +22,18 @@ public class MemberService {
                 .stream()
                 .map(MemberListResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Long save(MemberSignUpRequestDto requestDto) {
+        return memberRepository.save(requestDto.toEntity()).getMemberId();
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
+
+        memberRepository.deleteById(id);
     }
 }
