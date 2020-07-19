0e894ca0e0121e440da41f3afe908fe0f02ed253
sklim0932 <sklim0932@gmail.com>
2020-07-19 16:32:45 +0900

    DELETE MemberTests and moved findAll() test to MemberServiceTests

--- src/test/java/cml/theaters/moviereservation/service/member/MemberServiceTests.java
+++ src/test/java/cml/theaters/moviereservation/service/member/MemberServiceTests.java
@@ -2,11 +2,17 @@ package cml.theaters.moviereservation.service.member;
 
 import cml.theaters.moviereservation.domain.member.Member;
 import cml.theaters.moviereservation.domain.member.MemberRepository;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -15,10 +21,17 @@ public class MemberServiceTests {
     private MemberRepository memberRepository;
 
     @Test
+    public void member_findAll_test() {
+        System.out.println("===========");
+        List<Member> memberList = memberRepository.findAll();
+        assertNotNull(memberList);
+    }
+
+    @Test
     public void member_save_test() {
-        String name = "membertest";
-        String email = "membertest@membertest.com";
-        String password = "membertest";
+        String name = "savetest";
+        String email = "savetst@savetest.com";
+        String password = "savetest";
         String telNumber = "000-0000-0000";
 
         Member member = Member.builder()
