f1fc76d3cb958575b7ee1e7d4e0f5b0d8272d971
sklim0932 <sklim0932@gmail.com>
2020-07-31 23:37:37 +0900

    UPDATE MemberSignUpRequestDTO

--- src/main/java/cml/theaters/moviereservation/dto/MemberSignUpRequestDto.java
+++ src/main/java/cml/theaters/moviereservation/dto/MemberSignUpRequestDto.java
@@ -1,4 +1,34 @@
 package cml.theaters.moviereservation.dto;
 
+import cml.theaters.moviereservation.domain.member.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
 public class MemberSignUpRequestDto {
+    private String name;
+    private String email;
+    private LocalDateTime birthday;
+    private String telNumber;
+
+    @Builder
+    public MemberSignUpRequestDto(String name, String email, LocalDateTime birthday, String telNumber) {
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+        this.telNumber = telNumber;
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .name(this.name)
+                .email(this.email)
+                .birthday(this.birthday)
+                .telNumber(telNumber)
+                .build();
+    }
 }
